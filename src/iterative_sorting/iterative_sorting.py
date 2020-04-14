a4a2b470b9e812bb49fbc0e02bdb619301303f18
Dan Johnson <danimal.johnson@gmail.com>
2020-04-14 05:32:01 -0700

    Iterative sorting complete. Merge helper function.

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,30 +1,69 @@
-# TO-DO: Complete the selection_sort() function below 
-def selection_sort( arr ):
-    # loop through n-1 elements
+
+def selection_sort(arr):
+    # Ignore arrays that are too short.
+    if len(arr) <= 1:
+        return arr
+
+    # Loop through n-1 elements using i
     for i in range(0, len(arr) - 1):
         cur_index = i
         smallest_index = cur_index
-        # TO-DO: find next smallest element
-        # (hint, can do in 3 loc) 
-             
 
+        # Find next smallest element on the right
+        # of index i using cur_index
+        for cur_index in range(i+1, len(arr)):
+            if arr[cur_index] < arr[smallest_index]:
+                smallest_index = cur_index
 
+        # If a smaller value was found to the right, swap it with
+        # the current value.
+        if smallest_index != i:
+            temp = arr[i]
+            arr[i] = arr[smallest_index]
+            arr[smallest_index] = temp
 
-        # TO-DO: swap
-
-
-
+        #     print("Swapping {}({}) with {}({}): {}".format(
+        #         i, arr[i], smallest_index, arr[smallest_index], arr))
+        # else:
+        #     print("Skipping element {} ({})".format(
+        #         i, arr[i]))
 
     return arr
 
 
-# TO-DO:  implement the Bubble Sort function below
-def bubble_sort( arr ):
+def bubble_sort(arr):
+    # Ignore arrays that are too short
+    if len(arr) <= 1:
+        return arr
 
+    # Make 1 pass (p) for every element in the array.
+    for p in range(1, len(arr) - 1):
+        # print("checking from 0 to {}".format(len(arr) - p))
+        swaps_made = False
+
+        # The END of the list is sorted after each pass,
+        # so we subract p each time to get a shorter list.
+        for i in range(0, len(arr) - p):
+
+            # print("{}: arr[{}] = {}".format(arr, i, arr[i]))
+
+            # Start at index 0. Compare neighbors and swap larger
+            # items to the right until they "bubble" up to the end
+            # of the list.
+            if arr[i] > arr[i+1]:  # Swap
+                swaps_made = True
+                temp = arr[i]
+                arr[i] = arr[i+1]
+                arr[i+1] = temp
+        # Once we make a pass without any swaps, the list is sorted.
+        if swaps_made == False:
+            return arr
+
+    print("Error: Things didn't go as planned")
     return arr
 
 
 # STRETCH: implement the Count Sort function below
-def count_sort( arr, maximum=-1 ):
+def count_sort(arr, maximum=-1):
 
     return arr
--- src/recursive_sorting/recursive_sorting.py
+++ src/recursive_sorting/recursive_sorting.py
@@ -1,14 +1,54 @@
-# TO-DO: complete the helpe function below to merge 2 sorted arrays
-def merge( arrA, arrB ):
-    elements = len( arrA ) + len( arrB )
+# Helper function
+def merge(arrA, arrB):
+    elements = len(arrA) + len(arrB)
     merged_arr = [0] * elements
-    # TO-DO
-    
+    a_ptr = 0
+    b_ptr = 0
+    print("A has {} elements. B has {} elements.".format(len(arrA), len(arrB)))
+
+    # Step through the entire sorted array.
+    for i in range(0, len(merged_arr)):
+        print("i={}, a_ptr={}, b_ptr={}".format(
+            i, a_ptr, b_ptr))
+
+        # If we've already exhausted all values in array A,
+        # use the value in array B.
+        if a_ptr >= len(arrA):
+            print("A empty. Using B ({})".format(arrB[b_ptr]))
+            merged_arr[i] = arrB[b_ptr]
+            b_ptr += 1
+            print(merged_arr)
+
+        # Otherwise, if we've already exhausted all the values in array B,
+        # get the next value from array A.
+        elif b_ptr >= len(arrB):
+            print("B empty. Using A ({})".format(arrA[a_ptr]))
+            merged_arr[i] = arrA[a_ptr]
+            a_ptr += 1
+            print(merged_arr)
+
+        # Items remain in both arrays. Compare the first elements and use the lower one.
+        else:
+            print("Trying a_ptr={}, b_ptr={}".format(a_ptr, b_ptr))
+            if a_ptr >= len(a) or b_ptr >= len(b):
+                print("This probably shouldn't happen: {}".format(merged_arr))
+                continue
+            # Use the left array if arrA[0] is lower.
+            elif arrA[a_ptr] <= arrB[b_ptr]:
+                merged_arr[i] = arrA[a_ptr]
+                a_ptr += 1
+                print(merged_arr)
+            # Use the right array if arrB[0] is lower
+            else:
+                merged_arr[i] = arrB[b_ptr]
+                b_ptr += 1
+                print(merged_arr)
+
     return merged_arr
 
 
 # TO-DO: implement the Merge Sort function below USING RECURSION
-def merge_sort( arr ):
+def merge_sort(arr):
     # TO-DO
 
     return arr
@@ -20,14 +60,28 @@ def merge_in_place(arr, start, mid, end):
 
     return arr
 
-def merge_sort_in_place(arr, l, r): 
+
+def merge_sort_in_place(arr, l, r):
     # TO-DO
 
     return arr
 
+# Memoization:
+# from functools import lru_cache
+# @lru_cache(maxsize = 1000) # default = 128
+
 
 # STRETCH: implement the Timsort function below
 # hint: check out https://github.com/python/cpython/blob/master/Objects/listsort.txt
-def timsort( arr ):
+def timsort(arr):
 
     return arr
+
+
+a = [1, 2, 3]
+b = [4, 5, 6]
+c = [1, 3, 5]
+d = [2, 4, 6]
+e = [2, 4]
+f = [7]
+g = [4]
